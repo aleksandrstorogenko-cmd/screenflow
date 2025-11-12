19926ea54f10740b06bf8c0941a0c02752489ee1
Oleksandr Storozhenko <storogenko@ukr.net>
2025-11-11 17:18:36 -0800

    Initial Commit

---
+++ ScreenFlow/ScreenFlowApp.swift
@@ -0,0 +1,17 @@
+//
+//  ScreenFlowApp.swift
+//  ScreenFlow
+//
+//  Created by Oleksandr Storozhenko on 11/11/25.
+//
+
+import SwiftUI
+
+@main
+struct ScreenFlowApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
